4f5448bb9f4e233c56e91d1f5af7944c5fca56d3
NTCalkins <36932560+NTCalkins@users.noreply.github.com>
2020-02-18 00:19:03 -0800

    Add files via upload

--- models.py
+++ models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class State(models.Model):
+    state_name = models.CharField(max_length=25,unique=True)
+
+class City(models.Model):
+    city_name = models.CharField(max_length=50)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+class Address(models.Model):
+    address_name = models.CharField(max_length=50,unique=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    zipcode = models.CharField(max_length=5)
+
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -18,21 +31,30 @@ class Restaurant(models.Model):
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
     email = models.CharField(max_length=50,unique=True)
     phone = models.CharField(max_length=10,unique=True)
-    #TODO Opening Time
-    #TODO Closing Time
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
 
-class Address(models.Model):
-    address_name = models.CharField(max_length=50,unique=True)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
-    state = models.ForeignKey(State, on_delete=models.CASCADE)
-    zipcode = models.CharField(max_length=5)
+class Delivery(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, blank=True, null=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, blank=True, null=True)
+    is_fulfilled = models.BooleanField(default=False)
+    amount_payable = models.DecimalField(max_digits=8, decimal_places=2)
 
-class City(models.Model):
-    city_name = models.CharField(max_length=50,unique=True)
-    state = models.ForeignKey(State, on_delete=models.CASCADE())
+class FoodItem(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=140)
+    more_description = models.CharField(max_length=400)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    #TODO Image
+    is_vegetarian = models.BooleanField(default=False)
+    is_vegan = models.BooleanField(default=False)
 
-class State(models.Model):
-    state_name = models.CharField(max_length=25,unique=True)
+class FoodOrder(models.Model):
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
 
 class RatingChoices(models.IntegerChoices):
     ZERO = 0
@@ -43,19 +65,11 @@ class RatingChoices(models.IntegerChoices):
     FIVE = 5
 
 class Rating(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    ontime = models.IntegerField(choices=RatingChoices.choices)
-    quality = models.IntegerField(choices=RatingChoices.choices)
-
-class Product(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=140)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
-
-class ProductDetails(models.Model):
-
-class Order(models.Model):
-
-class OrderItem(models.Model):
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
+    driver_ontime = models.IntegerField(choices=RatingChoices.choices)
+    driver_quality = models.IntegerField(choices=RatingChoices.choices)
+    restaurant_ontime = models.IntegerField(choices=RatingChoices.choices)
+    restaurant_quality = models.IntegerField(choices=RatingChoices.choices)
