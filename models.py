7d25b4151d9187cfef22a84170a9eea3cae1fe36
marlenebarajas <48900002+marlenebarajas@users.noreply.github.com>
2020-02-19 08:43:13 -0800

    Minor models.py changes
    Added RestaurantCategories and a field for a restaurant to be categorized by them.

--- models.py
+++ models.py
@@ -1,38 +1,56 @@
 from django.db import models
 
+
 class State(models.Model):
-    state_name = models.CharField(max_length=25,unique=True)
+    state_name = models.CharField(max_length=25)
+
 
 class City(models.Model):
     city_name = models.CharField(max_length=50)
     state = models.ForeignKey(State, on_delete=models.CASCADE)
 
+
 class Address(models.Model):
-    address_name = models.CharField(max_length=50,unique=True)
+    address_name = models.CharField(max_length=50, unique=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     state = models.ForeignKey(State, on_delete=models.CASCADE)
     zipcode = models.CharField(max_length=5)
 
+
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=50,unique=True)
-    phone = models.CharField(max_length=10,unique=True)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    email = models.CharField(max_length=50, unique=True)
+    phone = models.CharField(max_length=10, unique=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, default=None)
+
 
 class Driver(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=50,unique=True)
-    phone = models.CharField(max_length=10,unique=True)
+    email = models.CharField(max_length=50, unique=True)
+    phone = models.CharField(max_length=10, unique=True)  # max length of a typical phone number
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, default=None)
+
+
+class RestaurantCategories(models.TextChoices):  # will allow restaurants to be categorized by specific food types
+    FAST = "Fast Food"
+    CHINESE = "Chinese Food"
+    MEXICAN = "Mexican Food"
+    ITALIAN = "Italian Food"
+    INDIAN = "Indian Food"
+    PIZZA = "Pizza"
+
 
 class Restaurant(models.Model):
     restaurant_name = models.CharField(max_length=50)
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
-    email = models.CharField(max_length=50,unique=True)
-    phone = models.CharField(max_length=10,unique=True)
+    email = models.CharField(max_length=50, unique=True)
+    phone = models.CharField(max_length=10, unique=True)
     opening_time = models.TimeField()
     closing_time = models.TimeField()
+    food_type = models.CharField(choices=RestaurantCategories.choices, default=RestaurantCategories.FAST)
+
 
 class Delivery(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -42,19 +60,25 @@ class Delivery(models.Model):
     is_fulfilled = models.BooleanField(default=False)
     amount_payable = models.DecimalField(max_digits=8, decimal_places=2)
 
+
 class FoodItem(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=140)
     more_description = models.CharField(max_length=400)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    #TODO Image
+    # TODO Image
     is_vegetarian = models.BooleanField(default=False)
     is_vegan = models.BooleanField(default=False)
+    is_glutenfree = models.BooleanField(default=False)
+
 
 class FoodOrder(models.Model):
-    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)
-    food_item = models.ForeignKey(FoodItem, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    delivery = models.ForeignKey(Delivery, on_delete=models.CASCADE)  # FK includes driver and restaurant info
+    food_item = models.ManyToManyField(FoodItem, on_delete=models.CASCADE)  # ManyToManyField so that we can include
+    # multiple food items to one order
+    quantity = models.IntegerField()  # this might be redundant
+    delivery_fee = models.DecimalField(max_digits=7, decimal_places=2)
+
 
 class RatingChoices(models.IntegerChoices):
     ZERO = 0
@@ -63,6 +87,7 @@ class RatingChoices(models.IntegerChoices):
     THREE = 3
     FOUR = 4
     FIVE = 5
+
 
 class Rating(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
