86e54d10741a170c2d057c7e4f85d082090d1f9c
kle97 <khoale@cpp.edu>
2020-03-29 16:56:43 -0700

    minor changes

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Khoa Le\Documents\PyCharm Projects\SlinkyLLC github\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
