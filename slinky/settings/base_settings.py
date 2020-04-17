3c06381aa1f19e08879de001d1820d6e0d931a9d
NTCalkins <NicholasTCalkins@gmail.com>
2020-04-16 17:57:29 -0700

    make it better

--- slinky/settings/base_settings.py
+++ slinky/settings/base_settings.py
@@ -16,7 +16,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# For static files
+# # For static files
 STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -35,10 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'bootstrap_modal_forms',
     # The following apps are required for allauth:
     'django.contrib.sites',
-
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
