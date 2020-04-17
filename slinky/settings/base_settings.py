605c5f78e3b2edd9010d6fe10acfd2a32fe76494
Matthew Levitt <39209018+M-Levitt@users.noreply.github.com>
2020-04-16 17:16:01 -0700

    Fixed a bug regarding static files not displaying in django

--- slinky/settings/base_settings.py
+++ slinky/settings/base_settings.py
@@ -16,6 +16,11 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# For static files
+STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
