f1b04b42805d33ec3bc2db4da252ebf214865cb7
Matthew Levitt <39209018+M-Levitt@users.noreply.github.com>
2020-04-19 23:47:30 -0700

    Routes for different links with React

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SignUpForm from './pages/SignUpForm'
-import SignInForm from './pages/SignInForm'
+import SignUpForm from './pages/SignUpForm';
+import SignInForm from './pages/SignInForm';
+import SemanticUI from './containers/SemanticUI';
+
 
 import CustomLayout from './containers/Layout';
+
+import {BrowserRouter as Router, Link} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <SignInForm />
-      </div>
+    <Router>
+    <ul>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    </ul>
+    <Route path="/" exact strict render={
+      () => {
+        return (<SemanticUI/>);
+      }
+    }/>
+    <Route path="/about" exact strict render={
+      () => {
+        return (<SignUpForm/>);
+      }
+    }/>
+    </Router>
     );
   }
 }
