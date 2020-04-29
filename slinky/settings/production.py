e0ae0b74f3c9aa8b45340e3acdccbdbe17174cc0
NTCalkins <NicholasTCalkins@gmail.com>
2020-04-29 16:47:26 -0700

    modified production.py

--- slinky/settings/production.py
+++ slinky/settings/production.py
@@ -12,7 +12,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'PASSWORD': config('PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
     }
