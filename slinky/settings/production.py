7e06e05c7449eeb486538cdf1108f5781a5f4dfc
NTCalkins <NicholasTCalkins@gmail.com>
2020-04-30 22:49:40 -0700

    gave env

--- slinky/settings/production.py
+++ slinky/settings/production.py
@@ -3,7 +3,7 @@ from decouple import config
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
